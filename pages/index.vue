9fa65f203e5f1c52775a84f7f969afd8c6ae1659
motakashi <takashi_home_sky@yahoo.co.jp>
2022-07-26 00:03:15 +0900

    axiosでデータを取得するよにする

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,21 @@
   <section>
     <h1>{{ message }}</h1>
     <router-link to="/another">anotherPage</router-link>
+    <p>{{users[0].name}}</p>
   </section>
 </template>
 
 <script>
+const axios = require('axios');
+let url = 'https://jsonplaceholder.typicode.com/users'
+
 export default {
+  asyncData({params}){
+    return axios.get(url)
+      .then((res) => {
+        return {users: res.data}
+      })
+  },
   data: function () {
     return {
       message: "Hello World",
